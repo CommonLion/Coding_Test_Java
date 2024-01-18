80edf4f7ab9b509d6acd5fe47531938bcdfd7b2c
CommonLion <74591574+CommonLion@users.noreply.github.com>
2024-01-18 14:13:11 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 772 ms, Memory: 37664 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -0,0 +1,63 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> list = new LinkedHashMap<>();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            String text = bf.readLine();
+            if(text.length()<length)continue;
+            if(list.get(text)!=null){
+                list.put(text, (list.get(text)+1));
+            }else list.put(text, 1);
+        }
+        bf.close();
+
+
+//        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(list.entrySet());
+//        entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
+//            @Override
+//            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+//                int lengthCompare = Integer.compare(o1.getKey().length(), o2.getKey().length());
+//                if (lengthCompare==0){
+//                    return o1.getValue().compareTo(o2.getValue());
+//                }
+//                //여기서 value의 값이 ㅜㅇ선으로 처리되고
+//                return lengthCompare;
+//            }
+//        });
+
+
+        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(list.entrySet());
+        entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                int valueCompare = o2.getValue().compareTo(o1.getValue());
+                if (valueCompare != 0) {
+                    return valueCompare;
+                }
+                int lengthCompare = Integer.compare(o2.getKey().length(), o1.getKey().length());
+                if (lengthCompare != 0) {
+                    return lengthCompare;
+                }
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
+
+
+        for(Map.Entry<String, Integer> entry : entryList){
+            bw.write(entry.getKey());
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
