165dbc30def08f414785451df489c5e75c93f95a
CommonLion <74591574+CommonLion@users.noreply.github.com>
2024-02-02 19:44:25 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18080 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,23 +2,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//바텀업 방식
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[n + 1]; // dp 배열 선언
-
-        // Bottom-up 방식으로 DP 테이블 채우기
+        int[] dp = new int[n + 1];
+        
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1; // 현재 수에서 1을 뺀 경우의 연산 횟수
+            /*1감소연산 먼저*/
+            dp[i] = dp[i - 1] + 1; 
 
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 2로 나누는 경우
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 3으로 나누는 경우
+            /*2와 3을 나눈 케이스를 각각 구함. 같은 i의 최솟값을 구하는 연산 포함*/
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1); 
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1); 
         }
-
-        System.out.println(dp[n]); // 결과 출력
+        System.out.println(dp[n]); 
     }
 }
