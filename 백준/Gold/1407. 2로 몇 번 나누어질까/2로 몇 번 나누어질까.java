b758fd2eb3c4409c4dee5f6be51cd402c7c55343
CommonLion <74591574+CommonLion@users.noreply.github.com>
2024-02-08 17:39:01 +0900

    [Gold IV] Title: 2로 몇 번 나누어질까, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long i = a;
+        long sum=0;
+
+        sum = two(b)- two(a-1);
+
+        System.out.println(sum);
+    }
+    static <list> long two(long x){
+        long count=0;
+        long sum=0;
+        long size=0;
+        long xx=x;
+        //Map<Long, Long> map = new LinkedHashMap<>();
+
+        ArrayList<Long> list = new ArrayList<>();
+        if(x==0) return 0;
+        if(x==1)return 1;
+
+        while(xx!=1){
+            xx/=2;
+            size++;
+        }
+        long []arr = new long[(int)size+1];
+        //System.out.println("size"+size);
+        int z=0;
+        arr[0]=x;
+        list.add(1L);
+        for (long i = 2; i <= x; i<<=1){
+            z++;
+            count = x/i;
+            arr[z]=count;
+            list.add(i);
+            //System.out.println("arr:"+arr[z]);
+        }
+       // System.out.println(list);
+        count = 0;
+        long temp=0;
+        //Collections.reverse(list);
+
+        for (int i = (int)size; i >= 0; i--) {
+            temp = arr[i] - count;
+            count += temp;
+            sum += temp*list.get(i);
+
+            //System.out.println(arr[i]+"t"+temp+"*"+count+"+"+sum);
+        }
+
+        return sum;
+    }
+
+    static long pow(long a, long n){
+        if (n == 0) return 1;
+        if(n==1)return a;
+        long tmp = pow(a, n/2);
+        //System.out.println(tmp);
+        if(n%2==0){
+            return tmp*tmp;
+        }else{
+            return a*tmp*tmp;
+        }
+    }
+
+
+}
